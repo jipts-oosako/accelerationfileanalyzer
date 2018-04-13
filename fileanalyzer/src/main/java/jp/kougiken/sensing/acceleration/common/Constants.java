e8abf22efc5d9a2837b70740fb4c0ff13710c6d6
syouhei_oosako <syouhei_oosako@cm.jip-ts.co.jp>
2018-04-13 15:44:54 +0900

    バージョン取得方法の変更、新バージョン設定

--- fileanalyzer/src/main/java/jp/kougiken/sensing/acceleration/common/Constants.java
+++ fileanalyzer/src/main/java/jp/kougiken/sensing/acceleration/common/Constants.java
@@ -8,6 +8,8 @@ import java.time.temporal.ChronoField;
  * Constant value - 定数値
  */
 public class Constants {
+	public static final String PRODUCT_VERSION = "1.1.0";
+
 	public static final int AXIS_X = 0;
 	public static final int AXIS_Y = 1;
 	public static final int AXIS_Z = 2;
